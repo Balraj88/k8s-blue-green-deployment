c0c114c4b1086b2cc00ffd010352153a9a43340c
guysalton-codefresh <guy.salton@codefresh.io>
2020-02-24 13:00:06 +0200

    move to green version

--- example/hello-web.go
+++ example/hello-web.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is the blue version")
+	fmt.Fprintf(w, "this is the green version")
 
 }
 
@@ -15,5 +14,4 @@ func main() {
 	fmt.Println("Trivial web server is starting on port 8080...")
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", nil)
-	os.Exit(3)
 }
